3bc96a3480f4cfd5ecaca92e2078f8e7277c14f6
Mark Mollineaux <bufordsharkley@gmail.com>
2016-01-18 21:16:15 -0700

    Cleaning up zsh update script. Pretty okay!

--- .oh-my-zsh/tools/check_for_upgrade.py
+++ .oh-my-zsh/tools/check_for_upgrade.py
@@ -4,63 +4,49 @@ import time
 import git
 
 ZSH_UPDATE_FILE = os.path.expanduser('~/.zsh-update')
-EPOCH_TARGET = -1
+DAYS_BETWEEN_CHECKS = 1
+COMMIT_QUERY = 'Type Y to commit and push these changes (d for diff):'
 
 
-def _current_epoch():
+def _current_epoch_days():
     return int(time.time() / (60 * 60 * 24))
 
 
 def _update_zsh_update():
     with open(ZSH_UPDATE_FILE, 'w') as f:
-        f.write('LAST_EPOCH={}\n'.format(_current_epoch()))
+        f.write('LAST_EPOCH={}\n'.format(_current_epoch_days()))
 
 
-def _upgrade_zsh():
-    _upgrade_script()
-    _update_zsh_update()
-
-
-class bcolors:
-        HEADER = '\033[95m'
-        OKBLUE = '\033[94m'
-        OKGREEN = '\033[92m'
-        WARNING = '\033[93m'
-        FAIL = '\033[91m'
-        ENDC = '\033[0m'
-        BOLD = '\033[1m'
-        UNDERLINE = '\033[4m'
+def print_color(message, color):
+    colors = {
+        'blue': '\033[94m',
+        'green': '\033[92m',
+        'FAIL': '\033[91m',
+        'normal': '\033[0m',
+    }
+    #HEADER = '\033[95m'
+    #WARNING = '\033[93m'
+    #BOLD = '\033[1m'
+    #UNDERLINE = '\033[4m'
+    print colors[color] + message + colors['normal']
 
 
 def _upgrade_script():
-    # Use colors, but only if connected to a terminal, and that terminal
-    # supports them.
-    """
-        if which tput >/dev/null 2>&1; then
-            ncolors=$(tput colors)
-
-    if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
-      RED="$(tput setaf 1)"
-      GREEN="$(tput setaf 2)"
-      YELLOW="$(tput setaf 3)"
-      BLUE="$(tput setaf 4)"
-      BOLD="$(tput bold)"
-      NORMAL="$(tput sgr0)"
-    """
-
-    print bcolors.OKBLUE + "Upgrading Dotfiles from github.com/bufordsharkley/dotfiles" + bcolors.ENDC
-    #gitpath = os.path.join(os.environ['DOTFILES'], 'dotfiles')
+    # TODO - use colors only if connected to a terminal, and colors supported
+    print_color('Upgrading Dotfiles from github.com/bufordsharkley/dotfiles',
+                'blue')
     repo = git.Repo(os.environ['DOTFILES'])
     if repo.is_dirty():
         print 'would you like to push?'
         repo.git.add(all=True)
         print repo.git.status()
         while True:
-            key = raw_input('Type Y to commit and push these changes (d for diff):').strip().lower()
+            key = raw_input(COMMIT_QUERY).strip().lower()
             if key == 'y':
-                commit_message = raw_input('Commit message?').strip()
+                commit_message = raw_input('Commit message:\n').strip()
                 repo.index.commit(commit_message)
                 _push_to_origin(repo)
+                break
             elif key == 'd':
                 print repo.git.diff(cached=True, color=True)
             else:
@@ -68,29 +54,30 @@ def _upgrade_script():
     else:
         repo.remotes.origin.pull()
 
+
 def _push_to_origin(repo):
     while True:
         try:
-            a = repo.remotes.origin.push()
-            print a
+            print repo.remotes.origin.push()
             return
         except git.exc.GitCommandError:
-            print bcolors.FAIL + 'You are a bad typer.' + bcolors.ENDC
+            print_color('You are a bad typer.', 'red')
 
 
-def _parse_zsh_update():
+def _fetch_time_of_last_update():
     file_contents = open(ZSH_UPDATE_FILE).read()
     try:
-      return int(file_contents.split('=')[1])
+        return int(file_contents.split('=')[1])
     except ValueError:
-      return 0
+        return 0
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     if os.path.exists(ZSH_UPDATE_FILE):
-        epoch = _parse_zsh_update()
-        diff = _current_epoch() - epoch
-        if diff > EPOCH_TARGET:
-            _upgrade_zsh()
+        last_update = _fetch_time_of_last_update()
+        diff = _current_epoch_days() - last_update
+        if diff > DAYS_BETWEEN_CHECKS:
+            _upgrade_script()
+            _update_zsh_update()
     else:
         _update_zsh_update()
