65b0d8be6ac226e70f823ee814f8defecf97e4dd
Mark Mollineaux <bufordsharkley@gmail.com>
2016-03-29 15:39:27 -0700

    doom now goes 1900-1920

--- doom.py
+++ doom.py
@@ -5,7 +5,8 @@ import time
 DAYS = dict(zip(('sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'), range(7)))
 REVERSE_DAYS = {v: k for k, v in DAYS.items()}
 
-JAN_ONE = datetime.date.today().replace(day=1, month=1).toordinal()
+JAN_ONE = datetime.date(1900, 1, 1).toordinal()
+TODAY = datetime.date.today()
 
 
 def get_guess():
@@ -17,21 +18,27 @@ def get_guess():
       pass
 
 
+def quiz_until_correct():
+  while True:
+    random_offset = random.randrange(0, 366 * 140)
+    rand_day = datetime.datetime.fromordinal(JAN_ONE + random_offset).date()
+    if rand_day > TODAY:
+      continue
+    print rand_day
+    weekday = rand_day.isoweekday()
+    guess = get_guess()
+    if weekday % 7 == guess % 7:
+      return
+    print 'No. Was {}'.format(REVERSE_DAYS[weekday % 7])
+
+
 def main():
   try:
     orig_time = time.time()
-    while True:
-      random_offset = random.randrange(0, 366)
-      rand_day = datetime.datetime.fromordinal(JAN_ONE + random_offset).date()
-      print rand_day
-      weekday = rand_day.isoweekday()
-      guess = get_guess()
-      if weekday % 7 == guess % 7:
-        break
-      print 'No. Was {}'.format(REVERSE_DAYS[weekday % 7])
-    print time.time() - orig_time
+    quiz_until_correct()
   except (EOFError, KeyboardInterrupt):
     pass
+  print time.time() - orig_time
 
 if __name__ == "__main__":
   main()
