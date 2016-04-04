87a3597c237fa9243f737bf159e74be2b26408e9
Mark Mollineaux <bufordsharkley@gmail.com>
2016-04-04 10:36:16 -0700

    Adding mute function to radio.

--- radio/radio.py
+++ radio/radio.py
@@ -1,4 +1,7 @@
+import os
 import sys
+import stat
+
 
 STATIONS = {'wfmu': 'http://wfmu.org/wfmu.pls',
             'kzsu': 'http://kzsu-streams.stanford.edu/kzsu-1-hi.mp3',
@@ -12,14 +15,30 @@ STATIONS = {'wfmu': 'http://wfmu.org/wfmu.pls',
 FIFO_FILE = '/tmp/radiofifo'
 
 
+def _is_a_fifo(path):
+  return stat.S_ISFIFO(os.stat(path).st_mode)
+
+
 def resolve_station_to_command(station):
   stream = STATIONS[station.lower()]
   return 'loadfile {}\n'.format(stream)
 
+def resolve_other_command(args):
+  if args[0] == 'mute':
+    return 'volume 0 1'
+  elif args[0] == 'unmute':
+    return 'volume 100 1'
+  else:
+    raise NotImplementedError
 
 def change_channel(args):
   try:
     command = resolve_station_to_command(args[0])
+  except KeyError:
+    command = resolve_other_command(args)
+  if not _is_a_fifo(FIFO_FILE):
+    raise RuntimeError(FIFO_FILE)
+  try:
     with open(FIFO_FILE, 'w') as f:
       f.write(command + '\n')
   except KeyError:
