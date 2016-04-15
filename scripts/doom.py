b1c2ffa5b702c6b3971a0296b2da7a4af4c2818d
Mark Mollineaux <bufordsharkley@gmail.com>
2016-04-15 12:02:04 -0700

    Adding extra format for date, and higher likelihood of upcoming year.

--- scripts/doom.py
+++ scripts/doom.py
@@ -6,7 +6,7 @@ DAYS = dict(zip(('sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'), range(7)))
 REVERSE_DAYS = {v: k for k, v in DAYS.items()}
 
 JAN_ONE = datetime.date(1900, 1, 1).toordinal()
-TODAY = datetime.date.today()
+TODAY = datetime.date.today().toordinal()
 
 
 def get_guess():
@@ -18,13 +18,26 @@ def get_guess():
       pass
 
 
+def random_date_format(date):
+  formats = ('%B %d, %Y', '%Y-%m-%d')
+  choice = random.sample(formats, 1)[0]
+  return date.strftime(choice)
+
+
+def random_date():
+  if random.random() < .3:
+    # Choose a date from the upcoming year:
+    random_offset = random.randint(0, 366)
+    return datetime.datetime.fromordinal(TODAY + random_offset).date()
+  else:
+    random_offset = random.randrange(0, 366 * 180)
+    return datetime.datetime.fromordinal(JAN_ONE + random_offset).date()
+
+
 def quiz_until_correct():
   while True:
-    random_offset = random.randrange(0, 366 * 180)
-    rand_day = datetime.datetime.fromordinal(JAN_ONE + random_offset).date()
-    #if rand_day > TODAY:
-    #  continue
-    print rand_day
+    rand_day = random_date()
+    print random_date_format(rand_day)
     weekday = rand_day.isoweekday()
     guess = get_guess()
     if weekday % 7 == guess % 7:
